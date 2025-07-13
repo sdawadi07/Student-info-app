bc00227c4bb79642128af3c61ea18ee56c630ec2
sdawadi07 <Swastikadawadi5865@gmail.com>
2025-07-12 22:52:25 -0600

    Displayed some student information

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+        public static void main(String[] args){
+            studentInfo s1 = new studentInfo("Swastika", 18, 3.5);
+            s1.displayInfo();
+        }
+    }
+
+
